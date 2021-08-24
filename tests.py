4edd435ce1d35dd99a741bc92fb275b5648c29c6
김상현 <llmm030506@naver.com>
2021-08-24 16:07:03 +0900

    update test

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ data = {
     "grant_type": "authorization_code",
     "client_id": "475eeac2ef5afa6af598830e6720ecc6",
     "redirect_uri": "https://localhost.com",
-    "code": "rR1NKPgUL_0fQ7y4fGlDgkKpESNJHofJUXtQVOV7hyOM9aufCNginWdSTZaSRWvuEiaLJAopyNoAAAF7YbMS5Q"
+    "code": ""
 
 }
 response = requests.post(url, data=data)
