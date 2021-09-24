879965c0dd57f0c0afe2b4fc24da2684f3f8256f
김상현 <llmm030506@naver.com>
2021-09-24 16:12:26 +0900

    terraform db 추가가

--- Terraform/src/terraform_web_infra.tf
+++ Terraform/src/terraform_web_infra.tf
@@ -27,3 +27,13 @@ resource "aws_instance" "web" {
     data.aws_security_group.default.id
   ]
 }
+
+resource "aws_db_instance" "web_db" {
+  allocated_storage   = 8
+  engine              = "mysql"
+  engine_version      = "5.6.35"
+  instance_class      = "db.t2.micro"
+  username            = "admin"
+  password            = "<DB_PASSWORD>"
+  skip_final_snapshot = true
+}
